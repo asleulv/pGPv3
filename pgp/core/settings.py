c27465c6b5d10c9fb7415f5ed8d43b129cb7afc9
asleulv <101048079+asleulv@users.noreply.github.com>
2024-10-04 19:58:57 +0200

    added env settings

--- pgp/core/settings.py
+++ pgp/core/settings.py
@@ -4,7 +4,7 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+DJANGO_ENV = config('DJANGO_ENV', default='development')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
