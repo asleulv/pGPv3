5bc7db379aa1696e03a0e2d5b07f122972d26a81
asleulv <101048079+asleulv@users.noreply.github.com>
2025-02-27 13:15:33 +0100

    Added 'forgot password' - removed "new round" item from menu

--- pgp/core/urls.py
+++ pgp/core/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),  # Includes the app URLs
-    path('accounts/', include('django.contrib.auth.urls')),  # Includes auth URLs
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
